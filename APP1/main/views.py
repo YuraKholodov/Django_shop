8b2f18e95a235b0c013feec83f2305df5f00921f
Yura Kholodov <yurdast@mail.ru>
2024-01-02 17:25:36 +0300

    02.01.24

--- APP1/main/views.py
+++ APP1/main/views.py
@@ -1,11 +1,19 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest
 from django.shortcuts import render
 
 
 def index(request: HttpRequest):
-    context = {"title": "Home", "content": "Главная страница магазина"}
+    context = {
+        "title": "Home - Главная",
+        "content": "Магазин мебели HOME",
+    }
     return render(request, "main/index.html", context=context)
 
 
 def about(request: HttpRequest):
-    return HttpResponse("About page")
+    context = {
+        "title": "Home - О нас",
+        "content": "О нас",
+        "text_on_page": "Текст о магазине, и почему надо покупать товары только в нем",
+    }
+    return render(request, "main/about.html", context=context)
