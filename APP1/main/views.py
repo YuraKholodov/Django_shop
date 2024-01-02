3e9be2db835c9cd6611d7f38fa2e12f581256ffc
Yura Kholodov <yurdast@mail.ru>
2024-01-02 16:12:58 +0300

    02.01.24

--- APP1/main/views.py
+++ APP1/main/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 
 
 def index(request: HttpRequest):
-    return HttpResponse("Home page")
+    context = {"title": "Home", "content": "Главная страница магазина"}
+    return render(request, "main/index.html", context=context)
 
 
 def about(request: HttpRequest):
