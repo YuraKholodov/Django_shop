735c9b3cb9ee8aedd9c7b053e7b6ac2c3fb501f2
Yura Kholodov <yurdast@mail.ru>
2024-01-04 16:13:50 +0300

    04.01.24

--- APP1/app/settings.py
+++ APP1/app/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "main",
+    "goods",
 ]
 
 MIDDLEWARE = [
@@ -93,7 +94,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ru"
 
 TIME_ZONE = "UTC"
 
--- APP1/main/views.py
+++ APP1/main/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpRequest
 from django.shortcuts import render
 
+from goods.models import Categories
+
 
 def index(request: HttpRequest):
+    categories = Categories.objects.all()
     context = {
         "title": "Home - Главная",
         "content": "Магазин мебели HOME",
+        "categories": categories,
     }
     return render(request, "main/index.html", context=context)
 
