759363ef3079b0e7b8598c2c050715223b46012b
Yura Kholodov <yurdast@mail.ru>
2024-01-09 14:12:37 +0300

    09.01.24

--- APP1/app/settings.py
+++ APP1/app/settings.py
@@ -66,8 +66,12 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'home',
+        "USER": 'home',
+        "PASSWORD": 'home',
+        "HOST": 'localhost',
+        "PORT": '5432',
     }
 }
 
@@ -107,14 +111,13 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
 INTERNAL_IPS = [
     "127.0.0.1",
-    ]
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
